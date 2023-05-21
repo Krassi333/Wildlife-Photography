29065dd0fc289923680b9408255a9c3dadc44ade
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-21 12:37:44 +0300

    Create home page html

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressConfig = require('./config/express');
+const databaseConfig = require('./config/database');
+const routesConfig = require('./config/routes');
+const port = 3000;
+
+async function start() {
+    const app = express();
+
+    await databaseConfig(app);
+    expressConfig(app);
+    routesConfig(app);
+
+    app.listen(port, () => console.log(`Server is listening on port ${port}`))
+}
+
+start();
