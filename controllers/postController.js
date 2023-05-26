89907441accc11d48215a8a6cfad160c04aff9b5
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-26 22:56:09 +0300

    Create add post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -18,4 +18,31 @@ router.get('/create', (req, res) => {
     });
 });
 
+router.post('/create', async (req, res) => {
+    const data = {
+        title: req.body.title,
+        keyword: req.body.keyword,
+        location: req.body.location,
+        createdAt: req.body.createdAt,
+        image: req.body.image,
+        description: req.body.description,
+        author: req.user._id,
+    };
+    console.log(data);
+
+    try {
+        await createPost(data);
+
+        res.redirect('/post/catalog');
+    } catch (err) {
+        const errors = errorParser(err);
+
+        res.render('create', {
+            title: 'Add Page',
+            data,
+            errors
+        });
+    }
+});
+
 module.exports = router;
