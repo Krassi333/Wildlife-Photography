cd5c045efa749709cf6e916328fba0d44d2931b4
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-27 22:34:22 +0300

    Create service to create post

--- services/postServices.js
+++ services/postServices.js
@@ -1,8 +1,27 @@
 const Post = require('../models/Post');
 const User = require('../models/User');
+
+
 async function getAllPosts() {
     return Post.find({}).collation({ locale: 'en', strength: 2 }).lean();
 }
+
+async function createPost(data) {
+    return Post.create(data);
+};
+
+async function getById(id) {
+    return Post.findById(id).collation({ locale: 'en', strength: 2 }).lean();
+}
+
+async function getPostAuthor(_id) {
+    return User.findOne({ _id });
+}
+
+async function deletePost(id) {
+    return Post.findByIdAndDelete(id);
+};
+
 module.exports = {
     getAllPosts,
     createPost,
